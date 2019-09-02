7926abd91a3472a8324446efffd491b1dbb6df88
aizenbergivan <46790744+aizenbergivan@users.noreply.github.com>
2019-09-02 17:08:56 +0300

    Version 2/0

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Privet!</h1>
+        <h1>Privet, future DevOPS!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
